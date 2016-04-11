9095b48787737c62e9e4700af2fd8ab9a1cec89e
Hedy Wang <hedywings@gmail.com>
2016-04-11 15:07:52 +0800

    wsServer waiting to complete

--- test/ut_wsClient.js
+++ test/ut_wsClient.js
@@ -1,28 +1,119 @@
 var WsClient = require('../lib/wsClient'),
-	WsServer = require('../lib/wsServer'),
-	should = require('should');
+    WsServer = require('../lib/wsServer'),
+    _ = require('lodash'),
+    util = require('util'),
+    EventEmitter = require('events').EventEmitter,
+    should = require('should');
+    port = process.env.PORT || 5000,
+    http = require('http'),
+    express = require('express'),
+    app = express();
+
+var server,
+    wsServer,
+    wsClient,
+    fb,
+    fbConstr = function () {
+        this.authorize = function (a, b) { b(null, true); };
+        this.authenticate = function (a, b, c) { c(null, true); };
+        this.dev = {};
+    };
+
+util.inherits(fbConstr, EventEmitter);
+fb = new fbConstr();
+
+app.use(express.static(__dirname + "/"))
+server = http.createServer(app)
+server.listen(port)
+wsServer = new WsServer(fb);
+wsServer.start(server);
 
 describe('Constructor Check', function () {
-	var wsClient = new WsClient();
+    wsClient = new WsClient();
 
-	it('WsClient()', function () {
+    it('WsClient()', function () {
         should(wsClient._wsClient).be.null();
         should(wsClient._auth).be.false();
         should(wsClient._connected).be.false();
         should(wsClient._nextTransId).be.Function();
-	});
+    });
+});
+
+describe('Signature Check', function () {
+    it('start(addr, options, authData)', function () {
+        // (function () { wsClient.start('ws://localhost:' + port, {}); }).should.not.throw();
+        // (function () { wsClient.start('ws://localhost:' + port, {}, {}); }).should.not.throw();
+
+        (function () { wsClient.start({}, {}); }).should.throw();
+        (function () { wsClient.start([], {}); }).should.throw();
+        (function () { wsClient.start(123, {}); }).should.throw();
+        (function () { wsClient.start(true, {}); }).should.throw();
+        (function () { wsClient.start(undefined, {}); }).should.throw();
+        (function () { wsClient.start('ws://localhost:' + port, []); }).should.throw();
+        (function () { wsClient.start('ws://localhost:' + port, 123); }).should.throw();
+        (function () { wsClient.start('ws://localhost:' + port, true); }).should.throw();
+        (function () { wsClient.start('ws://localhost:' + port, undefined); }).should.throw();
+        (function () { wsClient.start('ws://localhost:' + port, 'xxx'); }).should.throw();
+        (function () { wsClient.start('ws://localhost:' + port, {}, []); }).should.throw();
+        (function () { wsClient.start('ws://localhost:' + port, {}, 123); }).should.throw();
+        (function () { wsClient.start('ws://localhost:' + port, {}, true); }).should.throw();
+        (function () { wsClient.start('ws://localhost:' + port, {}, undefined); }).should.throw();
+        (function () { wsClient.start('ws://localhost:' + port, {}, 'xxx'); }).should.throw();
+    });
+
+    it('sendReq(subsys, cmd, args, callback)', function () {
+        (function () { wsClient.sendReq('xxx', 'xxx', {}, function () {}); }).should.not.throw();
+
+        (function () { wsClient.sendReq({}, 'xxx', {}, function () {}); }).should.throw();
+        (function () { wsClient.sendReq([], 'xxx', {}, function () {}); }).should.throw();
+        (function () { wsClient.sendReq(123, 'xxx', {}, function () {}); }).should.throw();
+        (function () { wsClient.sendReq(true, 'xxx', {}, function () {}); }).should.throw();
+        (function () { wsClient.sendReq(undefined, 'xxx', {}, function () {}); }).should.throw();
+        (function () { wsClient.sendReq('xxx', {}, {}, function () {}); }).should.throw();
+        (function () { wsClient.sendReq('xxx', [], {}, function () {}); }).should.throw();
+        (function () { wsClient.sendReq('xxx', 123, {}, function () {}); }).should.throw();
+        (function () { wsClient.sendReq('xxx', true, {}, function () {}); }).should.throw();
+        (function () { wsClient.sendReq('xxx', undefined, {}, function () {}); }).should.throw();
+        (function () { wsClient.sendReq('xxx', 'xxx', [], function () {}); }).should.throw();
+        (function () { wsClient.sendReq('xxx', 'xxx', 123, function () {}); }).should.throw();
+        (function () { wsClient.sendReq('xxx', 'xxx', true, function () {}); }).should.throw();
+        (function () { wsClient.sendReq('xxx', 'xxx', undefined, function () {}); }).should.throw();
+        (function () { wsClient.sendReq('xxx', 'xxx', 'xxx', function () {}); }).should.throw();
+        (function () { wsClient.sendReq('xxx', 'xxx', {}, {}); }).should.throw();
+        (function () { wsClient.sendReq('xxx', 'xxx', {}, []); }).should.throw();
+        (function () { wsClient.sendReq('xxx', 'xxx', {}, 123); }).should.throw();
+        (function () { wsClient.sendReq('xxx', 'xxx', {}, true); }).should.throw();
+        (function () { wsClient.sendReq('xxx', 'xxx', {}, undefined); }).should.throw();
+        (function () { wsClient.sendReq('xxx', 'xxx', {}, 'xxx'); }).should.throw();
+    });
 });
 
 describe('Functional Check', function () {
-	it('start()', function () {
-		
-	});
+    it('start()', function (done) {
+         wsClient.on('open', function () {
+            if (wsClient._connected && wsClient._auth)
+                done();
+         });
+         wsClient.start('ws://localhost:' + port, {});
+    });
 
-	it('stop()', function () {
+    it('sendReq()', function (done) {
+        wsClient.sendReq('dev', 'write', {value: 'kitchen'}, function (err, rsp) {
+            if (rsp.status === 1)
+                done();
+        });
+    });
 
-	});
+    it('stop()', function (done) {
+        var checkFlag = false;
 
-	it('sendReq()', function () {
+        wsClient.on('close', function (status) {
+            if (_.isNull(wsClient._wsClient) && wsClient._auth === false && wsClient._connected === false) 
+                checkFlag = true;
 
-	});
-})
+            if (status === 100 && checkFlag)
+                done();
+        });
+        wsClient.stop();
+    });
+});
---
+++ test/ut_wsServer.js
@@ -0,0 +1,142 @@
+var WsClient = require('../lib/wsClient'),
+    WsServer = require('../lib/wsServer'),
+    _ = require('lodash'),
+    ws = require('ws'),
+    util = require('util'),
+    EventEmitter = require('events').EventEmitter,
+    should = require('should');
+    port = process.env.PORT || 5000,
+    http = require('http'),
+    express = require('express'),
+    app = express();
+
+var server,
+    wsServer,
+    wsClient,
+    fb,
+    fbConstr = function () {
+        this.authorize = function (a, b) { b(null, true); };
+        this.authenticate = function (a, b, c) { c(null, true); };
+        this.dev = {};
+    };
+
+util.inherits(fbConstr, EventEmitter);
+fb = new fbConstr();
+
+app.use(express.static(__dirname + "/"))
+server = http.createServer(app)
+server.listen(port)
+
+describe('Constructor Check', function () {
+    wsServer = new WsServer(fb);
+
+    it('WsServer()', function () {
+        should(wsServer._fb).be.equal(fb);
+        should(wsServer._wsServer).be.null();
+        should(wsServer._wsClients).be.deepEqual([]);
+    });
+});
+
+describe('Signature Check', function () {
+    it('start()', function () {
+        (function () { wsServer.start().should.throw(); })
+    });
+});
+
+describe('Functional Check', function () {
+    it('start()', function (done) {
+
+       wsServer.start(server);
+        if (wsServer._wsServer instanceof ws.Server)
+            done();
+    });
+
+    it('isRunning()', function () {
+        wsServer.isRunning().should.be.true();
+    });
+
+    it('_initClient()', function (done) {
+        /*** test authenticate success ***/
+        wsClient = new WsClient();
+        wsClient.start('ws://localhost:' + port, {});
+
+        wsServer._wsServer.on('connection', function () {
+            setTimeout(function () {
+                var connClient = wsServer._wsClients[0]
+                if (connClient instanceof ws && connClient._auth)
+                    done();
+            }, 100);
+        });
+
+        /*** test authenticate error ***/
+        // wsServer._fb.authenticate = function (a, b, c) { c(null, false); }
+        // wsClient = new WsClient();
+        // wsClient.start('ws://localhost:' + port, {});
+
+        // wsServer._wsServer.on('connection', function () {
+        //     wsClient.on('close', function (status, data) {
+        //         if (status === 3001 && _.isEqual(wsServer._wsClients, []))
+        //             done();
+        //     });
+        // });
+    });
+
+    it('_reqHdlr()', function (done) {
+        wsClient.sendReq('dev', 'write', {value: 'kitchen'}, function (err, rspCode) {
+            if (rspCode.status === 1) done();
+        });
+    });
+
+    it('_sendRsp()', function (done) {
+        var reqMsg = {
+            __intf: 'REQ',
+            subsys: 'dev',
+            seq: 5,
+            id: 6,
+            cmd: 'write',
+            args: {value: 'kitchen'}
+        };
+
+        wsClient._wsClient.on('message', function (msg) {
+            var rspMsg = {
+                __intf: 'RSP',
+                subsys: 'dev',
+                seq: 5,
+                id: 6,
+                cmd: 'write',
+                status: 3,
+                data: 'unavailable'    
+            };
+
+            if (_.isEqual(JSON.parse(msg), rspMsg)) done();
+        });
+        wsServer._sendRsp(wsServer._wsClients[0], reqMsg, 3, 'unavailable');
+    });
+
+    it('_sendInd()', function (done) {
+        var nc = {
+                dump: function () { return {name: 'ble-core'} ;}
+            };
+
+        wsClient.on('permitJoining', function (msg) {
+            var indMsg = {
+                subsys: 'net',
+                id: null,
+                data: {
+                    netcore: 'ble-core',
+                    leftTime: 100
+                }
+            };
+
+            if (_.isEqual(msg, indMsg)) done();
+        });
+
+        fb.emit('permitJoin', nc, 100);
+    });
+
+    it('stop()', function () {
+        wsServer.stop();
+        should(wsServer._wsServer).be.null();
+        should(wsServer._wsClients).be.deepEqual({[]});
+    });
+});
